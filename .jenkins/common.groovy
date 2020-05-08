cf762c5a6e3ac7125a1d9e929f62c4620fd7a76b
amdkila <47991923+amdkila@users.noreply.github.com>
2020-05-08 17:03:39 -0600

    Add CI for hipFFT (#3)

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -1,92 +1,85 @@
 // This file is for internal AMD use.
 // If you are interested in running your own Jenkins, please raise a github issue for assistance.
+import static groovy.io.FileType.FILES
 
 def runCompileCommand(platform, project, jobName)
 {
     project.paths.construct_build_prefix()
-        
-    if(jobName.contains('hipclang'))
+
+    def getDependenciesCommand = ""
+    if (project.installLibraryDependenciesFromCI)
     {
-        command = """#!/usr/bin/env bash
-                set -x
-                cd ${project.paths.project_build_prefix}
-                LD_LIBRARY_PATH=/opt/rocm/lib CXX=g++ ${project.paths.build_command} --hip-clang
-                """
+        project.libraryDependencies.each
+        { libraryName ->
+            getDependenciesCommand += auxiliary.getLibrary(libraryName, platform.jenkinsLabel, 'develop')
+        }
     }
-    else
-    {
-        command = """#!/usr/bin/env bash
+
+    String cmake = platform.jenkinsLabel.contains('centos') ? "cmake3" : "cmake" 
+    String hipClang = platform.jenkinsLabel.contains('hipClang') ? "HIP_COMPILER=clang" : ""
+    def command = """#!/usr/bin/env bash
                 set -x
+                
+                ls /fftw/lib
+                export FFTW_ROOT=/fftw
+                export FFTW_INCLUDE_PATH=\${FFTW_ROOT}/include
+                export FFTW_LIB_PATH=\${FFTW_ROOT}/lib
+                export LD_LIBRARY_PATH=\${FFTW_LIB_PATH}:\$LD_LIBRARY_PATH
+                export CPLUS_INCLUDE_PATH=\${FFTW_INCLUDE_PATH}:\${CPLUS_INCLUDE_PATH}
+                export CMAKE_PREFIX_PATH=\${FFTW_LIB_PATH}/cmake/fftw3:\${CMAKE_PREFIX_PATH}
+                export CMAKE_PREFIX_PATH=\${FFTW_LIB_PATH}/cmake/fftw3f:\${CMAKE_PREFIX_PATH}
+                
                 cd ${project.paths.project_build_prefix}
-                LD_LIBRARY_PATH=/opt/rocm/hcc/lib CXX=g++ ${project.paths.build_command}
-                """
-    }
+                mkdir build && cd build
+                ${getDependenciesCommand}
+                LD_LIBRARY_PATH=/opt/rocm/lib:/opt/rocm/hip/lib ${hipClang} ${cmake} ${project.paths.build_command}
+            """
+
     platform.runCommand(this, command)
 }
 
-def runTestCommand (platform, project)
-{            
-    def command
-
-    if(platform.jenkinsLabel.contains('centos'))
-    {
-        command = """#!/usr/bin/env bash
-                set -x
-                cd ${project.paths.project_build_prefix}/build/release/clients/staging
-                LD_LIBRARY_PATH=/opt/rocm/hcc/lib sudo ./example-sscal
-                LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG sudo ./hipfft-test --gtest_output=xml --gtest_color=yes
+def runTestCommand (platform, project, gfilter)
+{
+    String sudo = auxiliary.sudo(platform.jenkinsLabel)
+    def command = """#!/usr/bin/env bash
+                    set -x
+                    cd ${project.paths.project_build_prefix}/build/clients/staging
+                    ${sudo} LD_LIBRARY_PATH=/opt/rocm/lib:/opt/rocm/hip/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG ./hipfft-test --gtest_output=xml --gtest_color=yes --gtest_filter=${gfilter}
                 """
-    }
-    else
-    {
-        command = """#!/usr/bin/env bash
-                set -x
-                cd ${project.paths.project_build_prefix}/build/release/clients/staging
-                LD_LIBRARY_PATH=/opt/rocm/hcc/lib ./example-sscal
-                LD_LIBRARY_PATH=/opt/rocm/hcc/lib GTEST_LISTENER=NO_PASS_LINE_IN_LOG ./hipfft-test --gtest_output=xml --gtest_color=yes
-                """
-    }
 
     platform.runCommand(this, command)
     junit "${project.paths.project_build_prefix}/build/release/clients/staging/*.xml"
 }
 
-def runPackageCommand(platform, project, jobName)
+def runPackageCommand(platform, project, jobName, label='')
 {
-    def command 
+    def command
 
-    if(platform.jenkinsLabel.contains('centos'))
-    {
-        command = """
-                set -x
-                cd ${project.paths.project_build_prefix}/build/release
-                make package
-                rm -rf package && mkdir -p package
-                mv *.rpm package/
-                rpm -qlp package/*.rpm
-            """
+    label = label != '' ? '-' + label.toLowerCase() : ''
+    ext = platform.jenkinsLabel.contains('ubuntu') ? "deb" : "rpm"
+    manager = platform.jenkinsLabel.contains('ubuntu') ? "dpkg -c" : "rpm -qlp"
 
-        platform.runCommand(this, command)
-        platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/release/package/*.rpm""")        
-    }
-    else if(jobName.contains('hipclang'))
-    {
-        packageCommand = null
-    }
-    else
-    {
-        command = """
-                set -x
-                cd ${project.paths.project_build_prefix}/build/release
-                make package
-                rm -rf package && mkdir -p package
-                mv *.deb package/
-                dpkg -c package/*.deb
-                """
+    command = """
+            set -x
+            cd ${project.paths.project_build_prefix}/build
+            make package
+            mkdir -p package
+            if [ ! -z "$label" ]
+            then
+                for f in hipfft*.${ext}
+                do
+                    echo f
+                    mv "\$f" "hipfft-internal${label}-\${f#*-}"
+                    ls
+                done
+            fi
+            mv *.${ext} package/
+            ${manager} package/*.${ext}
+        """
 
-        platform.runCommand(this, command)
-        platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/release/package/*.deb""")
-    }
+    platform.runCommand(this, command)
+    platform.archiveArtifacts(this, """${project.paths.project_build_prefix}/build/package/*.${ext}""")
 }
+
 
 return this
