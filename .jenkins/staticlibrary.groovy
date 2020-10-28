1ce8c214930caf3f0d7fa1c0a522226ac7f9dfd5
Steve Leung <Steve.Leung@amd.com>
2020-10-27 19:54:09 -0600

    cmake: add /opt/rocm/llvm to CMAKE_PREFIX_PATH, to work around static build issue
    * Add CI jenkinsfile to test static build
    
    Co-authored-by: Eiden Yoshida <eiden.yoshida@amd.com>

---
+++ .jenkins/staticlibrary.groovy
@@ -0,0 +1,102 @@
+#!/usr/bin/env groovy
+// This file uses an AMD internal Jenkins shared library. Please contact the maintainers if you have wish to enable Jenkins based continuous integration for this library.
+@Library('rocJenkins@pong') _
+
+// This is file for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+import com.amd.project.*
+import com.amd.docker.*
+import java.nio.file.Path
+
+def runCI =
+{
+    nodeDetails, jobName, buildCommand, label ->
+
+    def prj = new rocProject('hipFFT-internal', 'StaticLibrary')
+    // customize for project
+    prj.paths.build_command = buildCommand
+    prj.libraryDependencies = ['rocFFT-internal']
+
+    // Define test architectures, optional rocm version argument is available
+    def nodes = new dockerNodes(nodeDetails, jobName, prj)
+
+    def commonGroovy
+
+    boolean formatCheck = false
+
+    def compileCommand =
+    {
+        platform, project->
+
+        project.paths.construct_build_prefix()
+
+        commonGroovy = load "${project.paths.project_src_prefix}/.jenkins/common.groovy"
+        commonGroovy.runCompileCommand(platform, project, jobName, true)
+    }
+
+    def testCommand =
+    {
+        platform, project->
+
+        def gfilter = "*"
+        commonGroovy.runTestCommand(platform, project, gfilter)
+    }
+
+    def packageCommand =
+    {
+        platform, project->
+
+        commonGroovy.runPackageCommand(platform, project, jobName, label)
+    }
+
+    buildProject(prj, formatCheck, nodes.dockerArray, compileCommand, testCommand, packageCommand)
+}
+
+def setupCI(urlJobName, jobNameList, buildCommand, runCI, label)
+{
+    jobNameList = auxiliary.appendJobNameList(jobNameList)
+
+    jobNameList.each
+    {
+        jobName, nodeDetails->
+        if (urlJobName == jobName)
+            stage(label + ' ' + jobName) {
+                runCI(nodeDetails, jobName, buildCommand, label)
+            }
+    }
+
+    // For url job names that are not listed by the jobNameList i.e. compute-rocm-dkms-no-npi-1901
+    if(!jobNameList.keySet().contains(urlJobName))
+    {
+        properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * *')])]))
+        stage(label + ' ' + urlJobName) {
+            runCI([ubuntu16:['gfx906']], urlJobName, buildCommand, label)
+        }
+    }
+}
+
+ci: {
+    String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
+
+    def propertyList = ["compute-rocm-dkms-no-npi":[pipelineTriggers([cron('0 1 * * 0')])]]
+
+    propertyList = auxiliary.appendPropertyList(propertyList)
+
+    propertyList.each
+    {
+        jobName, property->
+        if (urlJobName == jobName)
+            properties(auxiliary.addCommonProperties(property))
+    }
+
+    def hostJobNameList = ["compute-rocm-dkms-no-npi-hipclang":([ubuntu18:['gfx900']])]
+
+    def hipClangJobNameList = ["compute-rocm-dkms-no-npi-hipclang":([ubuntu18:['gfx900']])]
+
+    String hostBuildCommand = '-DCMAKE_CXX_COMPILER=g++ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_CLIENTS_TESTS=ON -DBUILD_CLIENTS_SAMPLES=ON -DBUILD_SHARED_LIBS=OFF -L ../..'
+    String hipClangBuildCommand = '-DCMAKE_CXX_COMPILER=/opt/rocm/bin/hipcc -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_CLIENTS_TESTS=ON -DBUILD_CLIENTS_SAMPLES=ON -DBUILD_SHARED_LIBS=OFF -L ../..'
+
+    setupCI(urlJobName, hostJobNameList, hostBuildCommand, runCI, 'g++')
+    setupCI(urlJobName, hipClangJobNameList, hipClangBuildCommand, runCI, 'hip-clang')
+}
