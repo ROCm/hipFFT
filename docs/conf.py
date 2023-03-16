4a9839e40469498724a58d55450575d1fbd5d8ed
Sam Wu <sam.wu2@amd.com>
2023-03-16 17:18:37 -0600

    Documentation update for Read the Docs
    Standardize hipFFT documentation builds (#150)
    
    * standardize docs
    
    * update Doxyfile input
    
    * include src in Doxyfile input
    
    * remove old project logo
    
    * run custom command to remove HIPFFT_EXPORT from hipfft.h
    
    Add dependabot config and pin rocm-docs-core (#155)
    
    Co-authored-by: samjwu <samjwu@users.noreply.github.com>
    
    move dependabot config
    
    update bot config (#158)
    
    Update documentation requirements (#164)
    
    Co-authored-by: samjwu <samjwu@users.noreply.github.com>
    
    rocm-docs-core==0.13.4

---
+++ docs/conf.py
@@ -0,0 +1,18 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+from rocm_docs import ROCmDocs
+
+os.system("sed -e 's/HIPFFT_EXPORT //g' ../library/include/hipfft.h > .doxygen/hipfft.h")
+
+docs_core = ROCmDocs("hipFFT Documentation")
+docs_core.run_doxygen()
+docs_core.setup()
+
+
+for sphinx_var in ROCmDocs.SPHINX_VARS:
+    globals()[sphinx_var] = getattr(docs_core, sphinx_var)
