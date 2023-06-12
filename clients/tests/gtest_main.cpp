e4abc33bc34d2e989e9892cfa315fd71b94b46b4
Alan Ayala <alan.ayala@amd.com>
2023-06-12 10:59:36 -0400

    Updating rocFFT module and adding code for compatibility with latest rocFFT
    [ROCm/hipFFT commit: 467183e49a55e8a2f7754150af92621acf5e3ba0]

--- clients/tests/gtest_main.cpp
+++ clients/tests/gtest_main.cpp
@@ -50,6 +50,15 @@ namespace po = boost::program_options;
 // Control output verbosity:
 int verbose;
 
+// User-defined random seed
+size_t random_seed;
+
+// Probability of running individual planar FFTs
+double planar_prob;
+
+// Probability of running individual callback FFTs
+double callback_prob;
+
 // Transform parameters for manual test:
 fft_params manual_params;
 
@@ -197,23 +206,10 @@ void precompile_test_kernels(const std::string& precompile_file)
 
 int main(int argc, char* argv[])
 {
-    // NB: If we initialize gtest first, then it removes all of its own command-line
-    // arguments and sets argc and argv correctly; no need to jump through hoops for
-    // boost::program_options.
-    ::testing::InitGoogleTest(&argc, argv);
-
-    // Filename for fftw and fftwf wisdom.
-    std::string fftw_wisdom_filename;
-
-    // Filename for precompiled kernels to be written to
-    std::string precompile_file;
-
-    // Token string to fully specify fft params for the manual test.
-    std::string test_token;
-
+    // Parse arguments before initiating gtest.
     po::options_description opdesc(
         "\n"
-        "rocFFT Runtime Test command line options\n"
+        "hipFFT Runtime Test command line options\n"
         "NB: input parameters are row-major.\n"
         "\n"
         "FFTW accuracy test cases are named using these identifiers:\n"
@@ -230,13 +226,44 @@ int main(int argc, char* argv[])
         "      HP - hermitian planar\n"
         "\n"
         "Usage");
+
+    // clang-format does not handle boost program options very well:
+    // clang-format off
+   opdesc.add_options()
+        ("verbose,v",
+         po::value<int>()->default_value(0),
+         "print out detailed information for the tests.")
+        ("seed", po::value<size_t>(&random_seed),
+         "Random seed; if unset, use an actual random seed.")
+        ("callback_prob", po::value<double>(&callback_prob)->default_value(0.1),
+         "Probability of running individual callback transforms");
+    // clang-format on
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).options(opdesc).allow_unregistered().run(), vm);
+    po::notify(vm);
+
+    verbose = vm["verbose"].as<int>();
+
+    // NB: If we initialize gtest first, then it removes all of its own command-line
+    // arguments and sets argc and argv correctly; no need to jump through hoops for
+    // boost::program_options.
+    ::testing::InitGoogleTest(&argc, argv);
+
+    // Filename for fftw and fftwf wisdom.
+    std::string fftw_wisdom_filename;
+
+    // Token string to fully specify fft params for the manual test.
+    std::string test_token;
+
+    // Filename for precompiled kernels to be written to
+    std::string precompile_file;
+
     // Declare the supported options.
-    // clang-format doesn't handle boost program options very well:
+    // clang-format does not handle boost program options very well:
     // clang-format off
     opdesc.add_options()
         ("help,h", "produces this help message")
-        ("verbose,v",  po::value<int>()->default_value(0),
-        "print out detailed information for the tests.")
         ("skip_runtime_fails",  po::value<bool>(&skip_runtime_fails)->default_value(true),
         "Skip the test if there is a runtime failure.")
         ("transformType,t", po::value<fft_transform_type>(&manual_params.transform_type)
@@ -284,15 +311,11 @@ int main(int argc, char* argv[])
         ("wisdomfile,W",
          po::value<std::string>(&fftw_wisdom_filename)->default_value("wisdom3.txt"),
          "FFTW3 wisdom filename")
-        ("scalefactor", po::value<double>(&manual_params.scale_factor),
-         "Scale factor to apply to output.")
-        ("token", po::value<std::string>(&test_token)->default_value(""),
-         "Test token name for manual test")
-        ("precompile",  po::value<std::string>(&precompile_file),
-         "Precompile kernels to a file for all test cases before running tests");
+        ("scalefactor", po::value<double>(&manual_params.scale_factor), "Scale factor to apply to output.")
+        ("token", po::value<std::string>(&test_token)->default_value(""), "Test token name for manual test")
+        ("precompile",  po::value<std::string>(&precompile_file), "Precompile kernels to a file for all test cases before running tests");
     // clang-format on
 
-    po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, opdesc), vm);
     po::notify(vm);
 
@@ -309,8 +332,6 @@ int main(int argc, char* argv[])
         = vm.count("notInPlace") ? fft_placement_notinplace : fft_placement_inplace;
     if(vm.count("double"))
         manual_params.precision = fft_precision_double;
-
-    verbose = vm["verbose"].as<int>();
 
     if(vm.count("wise"))
     {
