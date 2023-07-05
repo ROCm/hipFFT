d2132165ae365ddbd1fcc0a80a9c4cec480b4f7f
Sam Wu <sam.wu2@amd.com>
2023-07-05 09:28:49 -0600

    remove dependabot

--- docs/conf.py
+++ docs/conf.py
@@ -7,12 +7,12 @@
 import os
 from rocm_docs import ROCmDocs
 
+
 os.system("sed -e 's/HIPFFT_EXPORT //g' ../library/include/hipfft.h > .doxygen/hipfft.h")
 
 docs_core = ROCmDocs("hipFFT Documentation")
 docs_core.run_doxygen()
 docs_core.setup()
-
 
 for sphinx_var in ROCmDocs.SPHINX_VARS:
     globals()[sphinx_var] = getattr(docs_core, sphinx_var)
