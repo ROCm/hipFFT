fdea58c6be4756f3b29f35e60bf35230ed960077
Sam Wu <22262939+samjwu@users.noreply.github.com>
2024-07-26 13:03:23 -0600

    Remove build command
    [ROCm/hipFFT commit: 0ea9ece517d2ae9555c81d80957bb86dd0c292ba]

--- .jenkins/staticanalysis.groovy
+++ .jenkins/staticanalysis.groovy
@@ -7,11 +7,10 @@ import java.nio.file.Path
 
 def runCI =
 {
-    nodeDetails, jobName, buildCommand ->
+    nodeDetails, jobName ->
 
     def prj = new rocProject('hipFFT-internal', 'PreCheckin')
     // customize for project
-    prj.paths.build_command = buildCommand
     prj.libraryDependencies = ['rocRAND','rocFFT']
 
     // Define test architectures, optional rocm version argument is available
@@ -25,10 +24,9 @@ def runCI =
 
 ci: { 
     String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
-    String hipClangBuildCommand = '-DCMAKE_CXX_COMPILER=/opt/rocm/bin/amdclang++ -DCMAKE_BUILD_TYPE=RelWithDebInfo -DBUILD_CLIENTS_TESTS=ON -DBUILD_CLIENTS_SAMPLES=ON -L ../..'
 
     properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * 7')])]))
     stage(urlJobName) {
-        runCI([ubuntu22:['any']], urlJobName, hipClangBuildCommand)
+        runCI([ubuntu22:['any']], urlJobName)
     }
 }
