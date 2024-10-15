3069d25208c21c769a2059849bb2b45310ee1931
Kevin Tran <Kevin.Tran@amd.com>
2024-10-15 16:11:33 -0600

    add fftw plan destroy and cleanup to simple test

--- clients/tests/simple_test.cpp
+++ clients/tests/simple_test.cpp
@@ -560,6 +560,8 @@ TEST(hipfftTest, RunR2C)
     nrmse /= maxv;
 
     EXPECT_TRUE(nrmse < type_epsilon<double>());
+    fftw_destroy_plan(ref_p);
+    fftw_cleanup();
     fftw_free(ref_out);
 }
 
@@ -637,5 +639,7 @@ TEST(hipfftTest, OutplaceOnly)
     nrmse /= maxv;
 
     ASSERT_TRUE(nrmse < type_epsilon<double>());
+    fftw_destroy_plan(ref_p);
+    fftw_cleanup();
     fftw_free(ref_out);
 }
