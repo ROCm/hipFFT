16b646590c644f9523e89d211e38fcf34cbd125e
Raphael Egan <Raphael.Egan@amd.com>
2025-06-28 05:20:24 +0000

    Bump up test probability for 'smoketest' options (still under 5 minutes)

--- clients/tests/gtest_main.cpp
+++ clients/tests/gtest_main.cpp
@@ -319,7 +319,7 @@ int main(int argc, char* argv[])
         ->each([&](const std::string&) {
             // The objective is to have an test that takes about 5 minutes, so just set the probability
             // per test to a small value to achieve this result.
-            test_prob = 0.002;
+            test_prob = 0.02;
         });
 
     // Try parsing initial args that will be used to configure tests
